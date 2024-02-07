fd7e01b04a4c2f6041e757c3299ab12691f96efd
Shahin <shahinibrahimli23@gmail.com>
2024-02-07 11:04:03 +0300

    Method for finding sum of odd indexes is added

--- lab0_Assignment2/assignment2.java
+++ lab0_Assignment2/assignment2.java
@@ -4,9 +4,7 @@ public class assignment2
 {
     public static void main(String[] args) 
     {
-        int [] array = createArray(10);
-        System.out.println(Arrays.toString(array));
-        System.out.println(sumOfOddIndex(array));
+        
     }
     public static int [] createArray(int size)
     {
@@ -19,12 +17,25 @@ public class assignment2
         return array;
     }
 
-    public static int sumOfOddIndex(int [] array)
+    public static int sumOfEvenIndex(int [] array)
     {
         int sum = 0;
         for (int i = 0; i < array.length; i++) 
         {
             if(i%2!=1)
+             {
+                sum = sum + array[i];
+             }
+        }
+        return sum;
+    }
+
+    public static int sumOfOddIndex(int [] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) 
+        {
+            if(i%2==1)
              {
                 sum = sum + array[i];
              }
