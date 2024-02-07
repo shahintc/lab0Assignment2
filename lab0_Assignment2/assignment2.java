fa6f7fe0ba30349e4d933852db3c248229c203f7
Shahin <shahinibrahimli23@gmail.com>
2024-02-07 10:37:50 +0300

    Array is made.

--- lab0_Assignment2/assignment2.java
+++ lab0_Assignment2/assignment2.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+import java.util.Random;
 public class assignment2 
 {
     public static void main(String[] args) 
     {
         
+    }
+    public static int [] createArray(int size)
+    {
+        int [] array = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) 
+        {
+            array[i] = rand.nextInt(101);
+        }
+        return array;
     }
 }
